1a864f6227c81d3ed030f513ae186433ab67a62e
Ruben van Staden <rubenvanstaden@gmail.com>
2021-11-26 11:32:35 -0500

    refactor: split into modules

---
+++ auth_fastapi/auth.py
@@ -0,0 +1,51 @@
+from jose import jwt
+from typing import Optional
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+import db
+
+
+SECRET_KEY = "51c15d357054acd2238db47deed0ff81e17c66a7369127cd0b4ce12ad5c57abc"
+ALGORITHM = "HS256"
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = db.get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
---
+++ auth_fastapi/db.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+fake_users_db = {
+    "rubenvs": {
+        "username": "rubenvs",
+        "full_name": "Ruben van Staden",
+        "email": "rubenvs@gmail.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
